526bdeb7a9e2df187e130e44517242a4c67d62cd
David Ingraham <davidingrahamf@gmail.com>
2024-05-21 13:34:58 -0400

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+//PascalCasing forreactcomponents
+
+function Message() {
+    //jsx syntax
+    const name ='Dave';
+    if (name)
+        return <h1> Hello {name}</h1>;
+    return <h1> Hello World</h1>;
+
+} 
+
+
+export default Message;
